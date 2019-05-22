ed298f2e3b243258e8fe39cca2e309b045ed71cb
schneidershades <schneidershades@gmail.com>
2019-05-22 23:00:53 +0100

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,47 +39,8 @@
                     </ul>
                     <!-- Tab panes -->
                     <div class="tab-content">
-                        <div role="tabpanel" class="tab-pane fade in active show" id="login">
-                            <form class="custom-form" id="loginform"  method="POST" action="{{ route('login') }}">
-                                @csrf
-                                <div class="row">
-
-                                    <div class="col-12">
-                                        <span class="fa fa-user"></span>
-                                        <input type="text" id="name1"  type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-                                        @if ($errors->has('email'))
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('email') }}</strong>
-                                            </span>
-                                        @endif
-                                    </div>
-                                    <div class="col-12">
-                                        <span class="fa fa-lock"></span>
-                                        <input type="password" id="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-                                        @if ($errors->has('password'))
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('password') }}</strong>
-                                            </span>
-                                        @endif
-                                    </div>
-
-
-                                    <div class="col-6">
-                                        <input type="checkbox" name="remember" id="checkbox_id1" value="value"  {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label for="checkbox_id1"> {{ __('Remember Me') }}</label>
-                                    </div>
-                                    <div class="col-6 text-right">
-                                        <a href="#" class="paswd"> forgot password ?</a>
-                                    </div>
-                                    <div class="col-12 mt-4">
-                                        <button type="submit" class="btn btn-one btn-anim w-100" id="submit-login" name="submit-login">
-                                            login</button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div role="tabpanel" class="tab-pane fade" id="register">
+                        
+                        <div role="tabpanel" class="tab-pane fade in active show" id="register">
                             <form class="custom-form" id="registerform" method="POST" action="{{ route('register') }}">
                                 @csrf
                                 <div class="row">
@@ -121,6 +82,47 @@
                                     <div class="col-12 mt-4">
                                         <button type="submit" class="btn btn-one btn-anim w-100" id="submit" name="submit">
                                             register</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                        <div role="tabpanel" class="tab-pane fade " id="login">
+                            <form class="custom-form" id="loginform"  method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="row">
+
+                                    <div class="col-12">
+                                        <span class="fa fa-user"></span>
+                                        <input type="text" id="name1"  type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                        @if ($errors->has('email'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="col-12">
+                                        <span class="fa fa-lock"></span>
+                                        <input type="password" id="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                        @if ($errors->has('password'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+
+
+                                    <div class="col-6">
+                                        <input type="checkbox" name="remember" id="checkbox_id1" value="value"  {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label for="checkbox_id1"> {{ __('Remember Me') }}</label>
+                                    </div>
+                                    <div class="col-6 text-right">
+                                        <a href="#" class="paswd"> forgot password ?</a>
+                                    </div>
+                                    <div class="col-12 mt-4">
+                                        <button type="submit" class="btn btn-one btn-anim w-100" id="submit-login" name="submit-login">
+                                            login</button>
                                     </div>
                                 </div>
                             </form>
